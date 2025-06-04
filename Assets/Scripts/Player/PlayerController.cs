35d68cb2fb831a48606a2f634cdffe6d59a338d3
shiyu <shiyu039@outlook.com>
2025-06-05 07:31:47 +0800

    角色移动

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -22,16 +22,95 @@ public class PlayerController : NetworkBehaviour
     /// <summary>
     /// 玩家移动相关
     /// </summary>
-    
+    private const float MoveSpeed = 1.7f;
+    private const float SprintSpeed = 3.3f;
+    private CharacterController characterController;
+    private float speed;
+    public float SpeedChangeRate = 10.0f;
+    private float animationBlend;
+    private float targetRotation;
+    private float rotationVelocity;
+    private float RotationSmoothTime = 0.12f;
+    private bool rotateOnMove = true;
+    private float verticalVelocity;
+    private GameObject mainCamera;
+    private Animator animator;
+
 
     private void Awake()
     {
         input = GetComponent<PlayerInputManager>();
         playerCameraRoot = GetComponentInChildren<PlayerCameraRoot>().transform;
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
     }
 
     private void Move()
     {
+        float targetSpeed = input.sprint ? SprintSpeed : MoveSpeed;
+
+        if (input.move == Vector2.zero) targetSpeed = 0.0f;
+
+        float currentHorizontalSpeed =
+            new Vector3(
+                characterController.velocity.x,
+                0.0f,
+                characterController.velocity.z
+            ).magnitude;
+
+        float speedOffset = 0.1f;
+
+        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
+                currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed,
+                Time.deltaTime * SpeedChangeRate);
+
+            speed = Mathf.Round(speed * 1000f) / 1000f;
+        }
+        else
+        {
+            speed = targetSpeed;
+        }
+        animationBlend = Mathf.Lerp(animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+        if (animationBlend < 0.01f) animationBlend = 0f;
+
+
+        Vector3 inputDirection = new Vector3(input.move.x, 0.0f, input.move.y).normalized;
+
+        if (input.move != Vector2.zero)
+        {
+            targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                                mainCamera.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity,
+                RotationSmoothTime);
+
+            if (rotateOnMove)
+            {
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
+
+
+        }
+
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
+
+        // Debug.Log(targetDirection.normalized * (speed * Time.deltaTime) +
+        //                     new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
+
+        Debug.Log(speed);
+
+        // move the player
+        characterController.Move(targetDirection.normalized * (speed * Time.deltaTime) +
+                            new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
+
+        animator.SetFloat("moveSpeed", animationBlend);
 
     }
 
@@ -52,6 +131,11 @@ public class PlayerController : NetworkBehaviour
         if (lfAngle < -360f) lfAngle += 360f;
         if (lfAngle > 360f) lfAngle -= 360f;
         return Mathf.Clamp(lfAngle, lfMin, lfMax);
+    }
+
+    private void Update()
+    {
+        if(IsOwner){ Move(); }
     }
 
     private void LateUpdate()
